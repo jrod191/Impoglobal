d7095ec927f989d0c220008abcc3cb4a59f4a6b5
jrod191 <jrod191@hotmail.com>
2020-09-05 16:45:10 -0400

    Se conecta a firebase y se crea el primer registro con exito

--- app/src/main/java/cl/grupo2/impoglobal/registro/domain/RegistrarUsuarioUseCase.kt
+++ app/src/main/java/cl/grupo2/impoglobal/registro/domain/RegistrarUsuarioUseCase.kt
@@ -1,8 +1,6 @@
 package cl.grupo2.impoglobal.registro.domain
 
-class RegistrarUsuarioUseCase(
-    private val registroUsuarioRepository : RegistroUsuarioRepository
-) {
-    suspend fun execute(registroUsuario: RegistroUsuario) =
-        registroUsuarioRepository.registro(registroUsuario)
+class RegistrarUsuarioUseCase(private val registroUsuarioRepository : RegistroUsuarioRepository) {
+
+    suspend fun execute(registroUsuario: RegistroUsuario) = registroUsuarioRepository.registro(registroUsuario)
 }
