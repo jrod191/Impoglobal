6363fc6bf91e8e3d9966989308ee16b585108e17
jrod191 <jrod191@hotmail.com>
2020-09-16 00:36:00 -0300

    Se configura catalogo y se deja funcionando con cardview

--- app/src/main/java/cl/grupo2/impoglobal/recyclerview/ProductosVH.kt
+++ app/src/main/java/cl/grupo2/impoglobal/recyclerview/ProductosVH.kt
@@ -3,10 +3,12 @@ package cl.grupo2.impoglobal.recyclerview
 import android.view.View
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.RecyclerView
 import cl.grupo2.impoglobal.R
 import cl.grupo2.impoglobal.databinding.ItemProductosBinding
 import cl.grupo2.impoglobal.productos.domain.model.Producto
+import com.bumptech.glide.Glide
 import com.squareup.picasso.Picasso
 import okhttp3.internal.notifyAll
 
@@ -22,6 +24,7 @@ class ProductosVH(
             tvCodigo.text = producto.codigo
             if (producto.imagen.isNotEmpty()) {
                 Picasso.get().load(producto.imagen).into(ivProducto)
+
             }
         }
         onItemClickProducto.onItemCLickProducto(producto)
